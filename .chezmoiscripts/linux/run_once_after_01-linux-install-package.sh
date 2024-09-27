1e64d0d7f162a87492261736820cbae43e1ecfca
MRDGH2821 <ask.mrdgh2821@outlook.com>
2024-09-27 16:50:48 +0530

    feat: combine bashrc from endeavour os, kde neon & fedora

--- .chezmoiscripts/linux/run_once_after_01-linux-install-package.sh
+++ .chezmoiscripts/linux/run_once_after_01-linux-install-package.sh
@@ -14,16 +14,3 @@ flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.f
 
 # Update rclone
 sudo rclone selfupdate
-
-# Enable Oh my posh's theme randomiser
-# shellcheck disable=SC2016
-omp_theme='
-# Oh my posh random theme initializer
-
-# Set a random theme
-themes=($(ls ~/.cache/oh-my-posh/themes/*.json))
-eval "$(oh-my-posh init bash --config "${themes[$RANDOM % ${#themes[@]}]}")"
-'
-if ! grep -q "${omp_theme}" "${HOME}/.bashrc"; then
-  echo "${omp_theme}" >>~/.bashrc
-fi

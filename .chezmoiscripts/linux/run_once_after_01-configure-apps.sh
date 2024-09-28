17762faa2b6dc5c92049fcbd12c1c857338f1484
MRDGH2821 <ask.mrdgh2821@outlook.com>
2024-09-28 10:27:00 +0530

    fix(script): :adhesive_bandage: fix conditionals & inform when done executing

--- .chezmoiscripts/linux/run_once_after_01-configure-apps.sh
+++ .chezmoiscripts/linux/run_once_after_01-configure-apps.sh
@@ -6,11 +6,11 @@ export PATH="${HOME}/.local/bin:${HOME}/bin:${PATH}"
 sudo rclone selfupdate
 
 # Add Cocogitto bash completition
-if ! type -P cog &>/dev/null; then
+if type -P cog &>/dev/null; then
+  mkdir -p ~/.local/share/bash-completion/completions
   cog generate-completions bash >~/.local/share/bash-completion/completions/cog
+  echo "Cocogitto bash completition installed"
 fi
 
 # Install Nerd font
 oh-my-posh font install meslo
-
-exit 0

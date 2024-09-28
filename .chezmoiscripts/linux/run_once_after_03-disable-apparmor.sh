244dbe4a414b2a8d4816eec7a2cf675f435a4103
MRDGH2821 <ask.mrdgh2821@outlook.com>
2024-09-28 10:34:06 +0530

    docs(script): :memo: change wording to reflect actual state of thing

--- .chezmoiscripts/linux/run_once_after_03-disable-apparmor.sh
+++ .chezmoiscripts/linux/run_once_after_03-disable-apparmor.sh
@@ -5,5 +5,4 @@ export PATH="${HOME}/.local/bin:${HOME}/bin:${PATH}"
 # Disable AppArmor for firefox
 sudo aa-disable firefox
 
-echo "AppArmor for Firefoxis now disabled"
-exit 0
+echo "Firefox AppArmor script executed."

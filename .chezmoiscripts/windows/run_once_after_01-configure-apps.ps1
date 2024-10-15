331f278273057d61e61d64f030a5d58901aae830
MRDGH2821 <ask.mrdgh2821@outlook.com>
2024-10-15 18:32:00 +0530

    feat(script): add post install ap configuring script for windows

--- .chezmoiscripts/windows/run_once_after_01-configure-apps.ps1
+++ .chezmoiscripts/windows/run_once_after_01-configure-apps.ps1
@@ -1,2 +1,8 @@
 # Install ggshield global precommit hook
 ggshield install -m global
+
+# Install Nerd font
+oh-my-posh font install meslo
+
+# Set gh as default git credentials helper
+gh auth setup-git

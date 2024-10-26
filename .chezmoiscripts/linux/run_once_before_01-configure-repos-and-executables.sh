87945eceb5660447d46494e914be371600d8a1ba
MRDGH2821 <ask.mrdgh2821@outlook.com>
2024-10-26 12:21:32 +0530

    refactor(script): :recycle: enclose chaotic-aur related commands in if block
    This prevents re-running same command if chaotic-aur repo is already present

--- .chezmoiscripts/linux/run_once_before_01-configure-repos-and-executables.sh
+++ .chezmoiscripts/linux/run_once_before_01-configure-repos-and-executables.sh
@@ -107,12 +107,12 @@ elif command -v dnf &>/dev/null; then
   rm -fr /tmp/dra
 
 elif command -v pacman &>/dev/null; then
-  sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
-  sudo pacman-key --lsign-key 3056513887B78AEB
-  sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
-  sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
-
   if ! grep -q '\[chaotic-aur\]' /etc/pacman.conf; then
+    sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+    sudo pacman-key --lsign-key 3056513887B78AEB
+    sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+    sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+
     sudo echo '[chaotic-aur]' | sudo tee -a /etc/pacman.conf
     sudo echo 'Include = /etc/pacman.d/chaotic-mirrorlist' | sudo tee -a /etc/pacman.conf
     echo "Chaotic AUR Repo added!"

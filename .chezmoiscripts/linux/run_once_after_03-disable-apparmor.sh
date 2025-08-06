08a7be518dac4192e418fe62789b197986500091
MRDGH2821 <ask.mrdgh2821@outlook.com>
2025-08-06 23:16:56 +1000

    feat(script): :hammer: early exit if AppArmor is not installed.

--- .chezmoiscripts/linux/run_once_after_03-disable-apparmor.sh
+++ .chezmoiscripts/linux/run_once_after_03-disable-apparmor.sh
@@ -3,6 +3,11 @@ set -e
 
 export PATH="${HOME}/.local/bin:${PATH}"
 
+if ! command -v aa-status &>/dev/null; then
+  echo "AppArmor is not installed."
+  exit 0
+fi
+
 # Disable AppArmor for firefox
 sudo aa-disable firefox
 

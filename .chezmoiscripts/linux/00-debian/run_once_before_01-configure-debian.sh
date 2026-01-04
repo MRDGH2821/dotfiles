04822f8d917903d833243c536e52b22e41224ae7
MRDGH2821 <ask.mrdgh2821@outlook.com>
2026-01-04 21:33:49 +1100

    feat(script): add a repository in debian script
    Signed-off-by: MRDGH2821 <ask.mrdgh2821@outlook.com>

--- .chezmoiscripts/linux/00-debian/run_once_before_01-configure-debian.sh
+++ .chezmoiscripts/linux/00-debian/run_once_before_01-configure-debian.sh
@@ -98,6 +98,10 @@ else
 fi
 echo "${LINE}"
 
+## debian.griffo.io - Unofficial Repository for Latest Development Tools
+curl -sS https://debian.griffo.io/EA0F721D231FDD3A0A17B9AC7808B4DD62C41256.asc | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/debian.griffo.io.gpg
+
+echo "deb https://debian.griffo.io/apt $(lsb_release -sc 2>/dev/null) main" | sudo tee /etc/apt/sources.list.d/debian.griffo.io.list
 
 # Install Soar
 curl -fsSL "https://raw.githubusercontent.com/pkgforge/soar/main/install.sh" | sh

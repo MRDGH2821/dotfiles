79f3c26c98a42b10e5bff9bc1735e8e528f5b773
MRDGH2821 <ask.mrdgh2821@outlook.com>
2026-05-04 16:28:09 +1000

    chore: update antidot config

--- dot_local/share/private_antidot/alias.sh
+++ dot_local/share/private_antidot/alias.sh
@@ -1,2 +1,1 @@
-#!/usr/bin/env bash
 alias wget="wget --hsts-file="${XDG_CACHE_HOME}/wget-hsts""
--- dot_local/share/private_antidot/env.sh
+++ dot_local/share/private_antidot/env.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/env bash
 export RUSTUP_HOME="${XDG_DATA_HOME}/rustup"
-export _JAVA_OPTIONS="-Djava.util.prefs.userRoot=\"${XDG_CONFIG_HOME}\"/java"
+export _JAVA_OPTIONS="-Djava.util.prefs.userRoot="$XDG_CONFIG_HOME"/java"
 export npm_config_cache="${XDG_CACHE_HOME}/npm"
 export CARGO_HOME="${XDG_DATA_HOME}/cargo"
 export GNUPGHOME="${XDG_DATA_HOME}/gnupg"
+export GOPATH="${XDG_DATA_HOME}/go"
 export GTK2_RC_FILES="${XDG_CONFIG_HOME}/gtk-2.0/gtkrc"
 export PSQL_HISTORY="${XDG_DATA_HOME}/psql_history"

7de9fe4af5091d684e280571378241bd554dfd08
MRDGH2821 <ask.mrdgh2821@outlook.com>
2026-06-25 10:14:26 +1000

    fix(script): fix order of exports

--- files/.chezmoitemplates/shell-scripts/00-export_paths.sh
+++ files/.chezmoitemplates/shell-scripts/00-export_paths.sh
@@ -4,21 +4,24 @@
 export PATH="${HOME}/AppImages:${PATH}"
 
 # XDG Base dirs
-eval "$(antidot init)" || true
 
 export XDG_STATE_HOME="${XDG_STATE_HOME:-${HOME}/.local/state}"
 export XDG_DATA_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}"
 export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-${HOME}/.config}"
 export XDG_CACHE_HOME="${XDG_CACHE_HOME:-${HOME}/.cache}"
 
+# soar
+export PATH="${XDG_DATA_HOME:-${HOME}/.local/share}/soar/bin:${PATH}"
+
+# antidot
+
+eval "$(antidot init)" || true
+
 # XDG folders
 export HISTFILE="${XDG_STATE_HOME}"/shell/history
 
 # bin
 export PATH="${HOME}/.local/bin:${PATH}"
-
-# soar
-export PATH="${XDG_DATA_HOME:-${HOME}/.local/share}/soar/bin:${PATH}"
 
 # fnm
 FNM_PATH="${XDG_DATA_HOME:-${HOME}/.local/share}/fnm"
